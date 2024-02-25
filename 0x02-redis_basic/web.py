4ccd34394bca6a769cf5495c3a01fbba0ea08bd4
PMagombedze <percymagoras@outlook.com>
2024-02-25 19:23:42 +0200

    cacher tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
 
-
-"""
-web
-"""
-
-
-import requests
 import redis
+import requests
+from functools import wraps
 
 
+r = redis.Redis()
+
+def URL(method):
+    """Decorator to track URL access count"""
+    @wraps(method)
+    def wrapper(url):
+        """Wrapper function to cache and count URL accesses"""
+        key = "cached:" + url
+        cache = r.get(key)
+        if cache:
+            return cache.decode("utf-8")
+
+        count = "count:" + url
+        content = method(url)
+
+        r.incr(count)
+        r.set(key, content, ex=10)
+        r.expire(key, 10)
+        return content
+    return wrapper
+
+@URL
 def get_page(url: str) -> str:
-    """get page"""
-    r = redis.Redis()
-    cached_data = r.get(url)
-    if cached_data is not None:
-        r.incr(f"count:{url}")
-        return cached_data.decode()
+    """Obtain the HTML content"""
+    results = requests.get(url)
+    return results.text
 
-    response = requests.get(url)
-    html_content = response.text
-    r.setex(url, 10, html_content)
-    r.set(f"count:{url}", 1)
-
-    return html_content
-
-print(get_page('http://slowwly.robertomurray.co.uk'))
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
