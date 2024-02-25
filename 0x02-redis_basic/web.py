364dca151b75093479e27f73d4bb0b37c09c5031
PMagombedze <percymagoras@outlook.com>
2024-02-25 19:26:35 +0200

    mypy

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
+
+"""
+web cacher and tracker
+"""
+
+
 import redis
 import requests
 from functools import wraps
 
-
 r = redis.Redis()
 
+
 def URL(method):
-    """Decorator to track URL access count"""
+    """url access count"""
     @wraps(method)
     def wrapper(url):
-        """Wrapper function to cache and count URL accesses"""
+        """wrapper function"""
         key = "cached:" + url
         cache = r.get(key)
         if cache:
@@ -26,11 +32,13 @@ def URL(method):
         return content
     return wrapper
 
+
 @URL
 def get_page(url: str) -> str:
-    """Obtain the HTML content"""
+    """obtain the HTML content"""
     results = requests.get(url)
     return results.text
+
 
 if __name__ == "__main__":
     get_page('http://slowwly.robertomurray.co.uk')
